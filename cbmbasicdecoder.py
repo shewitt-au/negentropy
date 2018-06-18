f7d69241b6e2957290bf1822bebbe2ec1021ba97
shewitt.au <shewitt.au@gmail.com>
2018-06-19 07:03:39 +1000

    Removed move of old BASIC parser stuff

--- cbmbasicdecoder.py
+++ cbmbasicdecoder.py
@@ -29,36 +29,6 @@ class BasicDecoder(decoders.Prefix):
 		# return the non-processed part of the interval
 		unproc = Interval(token.ivl.last+1, ivl.last) if token else ivl
 		return unproc
-			
-	def decode(self, ctx, ivl, params=None):
-		def lines():
-			def annotated_tokens():
-				# TODO: We should use 'params' to pass data in
-				for token in line_tokens(ctx.mem, livl, "c64font" in ctx.flags):
-					if token['type'] == 'line_ref':
-						target = line_to_address(ctx.mem, ivl, token['val'])
-						token['is_source'] = ctx.is_destination(target)
-						token['target'] = target
-					elif token['type'] == 'address':
-						target = token['val']
-						token['is_source'] = ctx.is_destination(target)
-						token['target'] = target
-					yield token
-
-			target_already_exits = params['target_already_exits']
-			for livl in line_iterator(ctx.mem, ivl):
-				yield {
-					'type': 'line',
-					'address': livl.first,
-					'tokens': annotated_tokens(),
-					'is_destination': (not target_already_exits) and (ctx.is_destination(livl.first))
-				}
-				target_already_exits = False
-
-		return {
-			'type': self.name,
-			'lines': lines()
-		}
 
 	def decode(self, ctx, ivl, params=None):
 		def lines():
