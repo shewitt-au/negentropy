e61195842d995196ce33673154e62e3db4fe78a9
shewitt.au <shewitt.au@gmail.com>
2018-07-12 19:37:17 +1000

    [NNP] Still coming to grips with Lark

--- scriptparser.py
+++ scriptparser.py
@@ -1,5 +1,32 @@
-from lark import Lark
+from lark import Lark, Transformer
 
+class ScriptTransformer(Transformer):
+	def datasource(self, t):
+		print(t)
+		length = len(t)
+		assert length==1 or length==2, "Syntax error"
+		if length==1:
+			p = (None, t[0])
+		else:
+			p = (str(t[0]), t[1])
+		print("datasource: ", p)
+		return p
+
+	def properties(self, t):
+		return {str(i[0]) : i[1] for i in t}
+	def propentry(self, t):
+		return t
+	def hexnum(self, t):
+		return int(t[0][1:], 16)
+	def decimal(self, t):
+		return int(t[0])
+	def string(self, t):
+		return str(t[0])
+	def boolean(self, t):
+		return bool(t[0])
+	def list(self, t):
+		return list(t)
+	
 if __name__=='__main__':
 	with open("MemType.txt", "r") as f:
 		t = f.read()
@@ -7,5 +34,5 @@ if __name__=='__main__':
 	l = Lark(open("script.lark").read(), parser='lalr')
 
 	t = l.parse(t)
-
-	print(t.pretty())
+	#print(t.pretty())
+	ScriptTransformer().transform(t)
