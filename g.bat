f84489f025264a4615c08a1f1e79450ed771044e
shewitt.au <shewitt.au@gmail.com>
2018-07-14 08:59:16 +1000

    [NNP] Removed old parser

--- g.bat
+++
@@ -1,2 +0,0 @@
-cls
-call antlr4 -Dlanguage=Python3 -o antlrparser config.g4
--- t.bat
+++
@@ -1,6 +0,0 @@
-cls
-call antlr4 -o javaantlrparser config.g4
-javac javaantlrparser/config*.java
-cd javaantlrparser
-call grun config r ../MemType.txt -gui
-cd ..
