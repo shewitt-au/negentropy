09b5f19ddd90789ed9a56531f25c984270d29b18
shewitt.au <shewitt.au@gmail.com>
2018-08-01 23:04:06 +1000

    [packaging] descriptions and stuff

--- setup.py
+++ setup.py
@@ -1,8 +1,26 @@
 from setuptools import setup
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(name='negentropy',
       version='0.1',
       description='C64 disassembler',
+      long_description=readme(),
+      long_description_content_type='text/markdown',
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Games/Entertainment',
+        'Topic :: Software Development :: Disassemblers',
+        'Topic :: Utilities'
+      ],
+      keywords='C64 disassembler 6502 6510',
       url='https://github.com/shewitt-au/negentropy',
       author='Stephen Hewitt',
       author_email='retrointernals@gmail.com',
