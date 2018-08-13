70b3204f09fca4c794e0f0d779ad491185a5d947
shewitt.au <shewitt.au@gmail.com>
2018-08-13 23:08:23 +1000

    PC relative addressing is useful outside of branches too

--- negentropy/M6502decoder.py
+++ negentropy/M6502decoder.py
@@ -46,7 +46,7 @@ class M6502Decoder(decoders.Prefix):
                         e = ctx.syms.lookup(ii.target, name_unknowns=False)
                         optext = e.name
                         if optext is None:
-                            if pc_rel and ii.op_info.mode==AddrMode.Relative:
+                            if pc_rel:
                                 optext = "*{:+}".format(e.addr-ii.ivl.first)
                             else:
                                 optext = format_numerical_operand(e.addr)
