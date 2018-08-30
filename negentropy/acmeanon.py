495e1b97a7de8a27f2652eb5feb0ae1591d13d0b
shewitt.au <shewitt.au@gmail.com>
2018-08-31 00:23:08 +1000

    RefInfo class: manages reference (initially just jumps, branches and subroutine calls) info

--- negentropy/acmeanon.py
+++ negentropy/acmeanon.py
@@ -19,5 +19,44 @@ class LowestFree(object):
 	def release(self, num):
 		self.free |= 1<<(num-1)
 
+class RefInfo(object):
+	def __init__(self, src, dst):
+		self.src = src
+		self.dst = dst
+
+	def offset(self):
+		return self.dst-self.src
+
+	def bottom(self):
+		return max(self.src, self.dst)
+
+	def __eq__(self, other):
+		return self.dst == other.dst
+	def __ne__(self, other):
+		return self.dst != other.dst
+	def __lt__(self, other):
+		return self.dst < other.dst
+	def __le__(self, other):
+		return self.dst <= other.dst
+	def __gt__(self, other):
+		return self.dst > other.dst
+	def __ge__(self, other):
+		return self.dst >= other.dst
+
+	def __repr__(self):
+		o = self.offset()
+		if o>0:
+			return "RefInfo(${:04x}-->${:04x})".format(self.src, self.dst)
+		elif o<0:
+			return "RefInfo(${:04x}<--${:04x})".format(self.dst, self.src)
+		else:
+			return "RefInfo(${0:04x}---${0:04x})".format(self.src)
+
 if __name__=='__main__':
-	pass
+	r1 = RefInfo(0, 10)
+	r2 = RefInfo(20, 11)
+	r3 = RefInfo(12, 12)
+	l = [r1, r2, r3]
+	l.sort()
+	print(l)
+	print(r1.bottom())
